2bf89bf920cef6e51ca9952189720f6ec7ff803f
m-woo <mark.chi.woo@gmail.com>
2017-03-06 21:21:17 -0800

    created user database

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var ejsLayouts = require('express-ejs-layouts')
 var session = require('express-session');
 var flash = require('connect-flash');
 // var isLoggedin = require('./middleware/isLoggedin')
-var passport = require('./config/passportConfig');
-require('dotenv').config();
+// var passport = require('./config/passportConfig');
+// require('dotenv').config();
 
 var port = 3000;
 
@@ -15,19 +15,19 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 app.use(express.static(__dirname + '/public/'));
-app.use(session({
-	secret: process.env.SESSION_SECRET,
-	resave: false,
-	saveUninitialized: true
-}));
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(function(req, res, next){
-	res.locals.currentUser = req.user;
-	res.locals.alerts = req.flash();
-	next();
-});
+// app.use(session({
+// 	secret: process.env.SESSION_SECRET,
+// 	resave: false,
+// 	saveUninitialized: true
+// }));
+// app.use(flash());
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(function(req, res, next){
+// 	res.locals.currentUser = req.user;
+// 	res.locals.alerts = req.flash();
+// 	next();
+// });
 
 
 
