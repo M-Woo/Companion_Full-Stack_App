c3ea01a7d291f355256a6e59831c6bac592e483f
m-woo <mark.chi.woo@gmail.com>
2017-03-10 15:09:04 -0800

    socket.io working error fix

--- public/chat.js
+++ public/chat.js
@@ -17,7 +17,7 @@ console.log('chat.js works')
             
                 var html = '';
             for(var i=0; i<messages.length; i++) {
-                html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
+                html += '<b>' + (messages[i].username ? messages[i].username : 'Jesus') + ': </b>';
                 html += messages[i].message + '<br />';
             }
             content.innerHTML = html;
@@ -28,6 +28,7 @@ console.log('chat.js works')
 
 sendButton.onclick = sendMessage = function() {
     console.log("i was clicked");
+    $("#content").scrollTop($("#content")[0].scrollHeight);
     $.ajax({
             method: "GET", 
             url:"/markov/one"
